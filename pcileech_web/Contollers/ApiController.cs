9c0076df12bfd395d3a1e5988f5ece34fa0ee671
lidong.lv <lidong.lv@westsnow.cn>
2024-07-05 14:28:36 +0800

    fixed linux crash

--- pcileech_web/Contollers/ApiController.cs
+++ pcileech_web/Contollers/ApiController.cs
@@ -12,7 +12,7 @@ namespace pcileech_web.Contollers
         private delegate void OnProgressNotify(ulong success, ulong fail, ulong total,ulong speed);
 
         [DllImport("pcileech.dll")]
-        static extern int StartDump(OnProgressNotify opn,string outPath);
+        static extern int StartDump(OnProgressNotify opn,string outPath,int only700,ulong pmax);
 
         [DllImport("pcileech.dll")]
         static extern void StopDump();
@@ -23,12 +23,53 @@ namespace pcileech_web.Contollers
 
         public async Task<IActionResult> DumpMemory()
         {
+            ulong pmax = 0;
+            if (Request.Query.ContainsKey("pmax"))
+            {
+                var pmaxStr = Request.Query["pmax"].ToString();
+                if (pmaxStr.ToLower().StartsWith("0x"))
+                {
+                    ulong.TryParse(pmaxStr.Substring(2), System.Globalization.NumberStyles.HexNumber, null, out pmax);
+                }
+                else
+                {
+                    ulong.TryParse(pmaxStr, out pmax);
+                }
+            }
+            int only700 = 0;
+            if (Request.Query.ContainsKey("only700"))
+            {
+                var only700Str = Request.Query["only700"].ToString();
+                int.TryParse(only700Str, out only700);
+            }
+
             var dic=new Dictionary<string,object>();
             var status = 0;//0 ok 1 failed 2 running
             if(Interlocked.CompareExchange(ref _status, 1, 0) == 0)
             {
                 try
                 {
+
+                    //int maxRootLength = 0;
+                    //DriveInfo[] drives = DriveInfo.GetDrives();
+                    //DriveInfo selectDrive = null;
+                    //foreach (var drive in drives)
+                    //{
+                    //    if (drive.RootDirectory.Name.Length > maxRootLength && Environment.CurrentDirectory.StartsWith(drive.RootDirectory.Name))
+                    //    {
+                    //        maxRootLength = drive.RootDirectory.Name.Length;
+                    //        selectDrive = drive;
+                    //    }
+                    //}
+                    //if (selectDrive == null)
+                    //{
+
+                    //}
+                    //else
+                    //{
+                    //    selectDrive.AvailableFreeSpace/selectDrive.TotalSize
+                    //}
+
                     Response.ContentType = "text/event-stream; charset=utf-8";
                     Response.Headers["Cache-Control"] = "no-cache";
                     await Response.StartAsync();
@@ -56,7 +97,7 @@ namespace pcileech_web.Contollers
                         name += Request.Query["name"];
                     }
                     name += ".raw";
-                    status = StartDump(opn, "mem/" + name);
+                    status = StartDump(opn, "mem/" + name, only700, pmax);
 
                     dic["status"] = status;
                     sw.WriteLine(JsonConvert.SerializeObject(dic));
